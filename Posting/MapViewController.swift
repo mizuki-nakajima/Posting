09948b08ff4a62d77138698e7fdf4f869249df0b
mizuki.nakajima <m173305s@st.u-gakugei.ac.jp>
2019-03-19 03:29:14 +0900

    現在地周辺表示まで

--- Posting/MapViewController.swift
+++ Posting/MapViewController.swift
@@ -10,15 +10,56 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapViewController: UIViewController,CLLocationManagerDelegate{
+class MapViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelegate{
 
     @IBOutlet weak var mapView: MKMapView!
+    //CLLocationManagerを定義
+    var myLocationManager:CLLocationManager!
+    //タップされた回数
+    var tapped = 1
+    //ロングタップしたときに立てるピンを定義
+    var pinByLongPress:MKPointAnnotation!
     
+    //var pinTintColor:UIColor!
+    
+    //ロングタップされたときに呼ばれるメソッド
+    @IBAction func longPressMap(_ sender: UILongPressGestureRecognizer) {
+        //ロングタップの最初の感知のみ受け取る
+        if(sender.state != UIGestureRecognizer.State.began){
+            return
+        }
+        //ロングタップを検出したことと回数をログに表示
+        print("DEBUG_PRINT : long tapped \(tapped)")
+        tapped += 1
+        
+        //インスタンス化
+        pinByLongPress = MKPointAnnotation()
+        
+        //ピンの色を設定
+        //mapView?.pinTintColor = UIColor.purpleColor()
+       
+        
+        //ロングタップから位置情報を取得
+        let location:CGPoint = sender.location(in: mapView)
+        
+        //取得した位置情報をCLLocationCoordinate2D（座標）に変換
+        let longPressedCoordinate:CLLocationCoordinate2D = mapView.convert(location, toCoordinateFrom: mapView)
+        
+        
+        
+        //ロングタップした位置の座標をピンに入力
+        pinByLongPress.coordinate = longPressedCoordinate
+        print("DEBUG_PRINT : \(longPressedCoordinate)")
+        //ピンを追加する（立てる）
+        mapView.addAnnotation(pinByLongPress)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //現在地周辺を表示する
+        self.mapView.showsUserLocation = true
+        self.mapView.userTrackingMode = .followWithHeading
     }
     
 
@@ -30,6 +71,8 @@ class MapViewController: UIViewController,CLLocationManagerDelegate{
         self.present(tabBarViewController!, animated: true, completion: nil)
     }
     
+    
+    
     /*
     // MARK: - Navigation
 
@@ -39,5 +82,7 @@ class MapViewController: UIViewController,CLLocationManagerDelegate{
         // Pass the selected object to the new view controller.
     }
     */
+    
 
+    
 }
